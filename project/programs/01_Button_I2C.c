60cfc6fc94dcc1719ec79e82aeee8d5e8e5aeb18
ruixiang-shaop <ruixiang.shaop@um.es>
2021-07-05 22:33:18 +0200

    Added circuitry explanation for I2C buses

--- project/programs/01_Button_I2C.c
+++ project/programs/01_Button_I2C.c
@@ -120,6 +120,9 @@ void Init_GPIO_Leds(void)
 
 // I2C2_SCL PF1
 // I2C2_SDA PF0
+// Each pin (master and slave) must be connected to the same bus, one for SCL and another for SDA
+// Each bus must be powered by VCC (3.3V) with a resistor of 5kOhm
+
 
 void Init_I2C2(void)
 {
